37f4da9de3e99aaeceeedbbb46497bb70f4e5af0
Juan Miguel Sánchez Arce <jumisan134@gmail.com>
2021-03-29 23:43:11 +0200

    Update comment serializer

--- backend/comments/serializers.py
+++ backend/comments/serializers.py
@@ -2,12 +2,12 @@ from rest_framework import serializers
 
 from comments.models import Comment
 from reviews.serializers.serializers_basic import ReviewSerializerWithAuthor
-from users.serializers.serializers_main import UserSerializerBasic
+from users.serializers.serializers_basic import UserSerializerBasic
 
 
 class CommentSerializer(serializers.ModelSerializer):
     commented_by = UserSerializerBasic(read_only=True)
-    liked_by = UserSerializerBasic(read_only=True)
+    liked_by = UserSerializerBasic(read_only=True, many=True)
     review = ReviewSerializerWithAuthor
 
     class Meta:
@@ -17,9 +17,8 @@ class CommentSerializer(serializers.ModelSerializer):
 
 class CommentSerializerBasic(serializers.ModelSerializer):
     commented_by = UserSerializerBasic(read_only=True)
+    liked_by = UserSerializerBasic(read_only=True, many=True)
 
     class Meta:
         model = Comment
         fields = ['id', 'review', 'commented_by', 'comment_content', 'liked_by']
-
-

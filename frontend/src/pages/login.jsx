c722ef0ad244f84878b197c614d33f9f41c388d4
erhan <the.software.coder@gmail.com>
2021-03-29 16:57:00 +0200

    Login finished

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,98 +1,17 @@
 import React from 'react';
-import styled from 'styled-components';
-
-
-// ######################### Styling  ################################
-
-
-const MainContainer = styled.div `
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
-    width: 100%;
-    justify-content: space-evenly;
-    align-items: center;
-    background: #F2F2F2;
-
-    button {
-        background-color: orange;
-        padding: 15px 30px;
-        width: 200px;
-        align-self: center;
-        border-radius: 100px;
-        color: #fff;
-        border: none;
-        font-size: 1rem;
-        text-transform: uppercase;
-        letter-spacing: 1px;
-        cursor: pointer;
-        transition: all 0.3s;
-
-        :hover {
-            box-shadow: 2px 5px 10px rgba(0, 0, 0, 0.4);
-            transform: scale(1.05) translateY(-5px);
-      }
-    }
-
-    .inputField {
-        width: 50%;
-        height: 40vh;
-        display: flex;
-        flex-direction: column;
-        
-        .inputContainer {
-            display: flex;
-            align-items: center;
-            border-bottom: 1.5px solid #e5e5e5;
-            padding: 10px 0;
-            margin-bottom: 80px;
-        }   
-        #passwordContainer {
-            margin-bottom: 80px;
-            margin-top: 20px;
-        }
-        img {
-            height: 25px;
-            width: 25px;
-            margin-right: 15px;
-        }
-        input {
-            padding: 10px;
-            border: none;
-            outline: none;
-            font-size: 1rem;
-            ::placeholder {
-                font-size: 1rem;
-        }
-    }
-    }
-`;
-
-const LoginTitle = styled.div`
-  margin-bottom: 20px;
-  font-size: 40px;
-  text-align: center;
-  color: black;
-  font-family: Roboto, sans-serif;
-  /* position: absolute; */
-  width: 357px;
-  height: 46px;
-  left: 540px;
-  top: 120px;
-`;
-
-
+import HeaderNavi from '../components/headerNavi/index';
+import { InputField, LoginTitle, LoginWrapper, MainContainer } from '../styles/pages/loginStyles';
 
 //############################# Component ################################
 
 const LoginPage = () => {
     return (
-        <>
         <MainContainer>
-            <LoginTitle>LOGIN</LoginTitle>
-            <inputField>
-                <div className='inputContainer' style={{marginBottom:'1em'}}>
+            <HeaderNavi/>
+            <LoginWrapper> 
+                <LoginTitle>LOGIN<span></span> </LoginTitle>
+                    <span></span>
+                <InputField>
                     <input
                         required
                         // onChange={}
@@ -100,11 +19,7 @@ const LoginPage = () => {
                         name='username'
                         type='text'
                         placeholder='Username'
-                        style={{height:'2em',border:'none'}}
                     />
-                </div>
-                <br></br>
-                <div className='inputContainer' id='passwordContainer'>
                     <input
                         required
                         // onChange={}
@@ -113,13 +28,10 @@ const LoginPage = () => {
                         type='password'
                         placeholder='Password'
                     />
-                </div>
-
-            </inputField>
-
-            <button type='submit'>Login</button>
+                </InputField>
+                <button type='submit'>Login</button>
+            </LoginWrapper>
         </MainContainer>            
-        </>
     )
 }
 

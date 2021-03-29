41383410f21a5915169a68aa74a9aa8a834d68d5
justina <justynazurawska@icloud.com>
2021-03-29 19:33:50 +0200

    comments likes endpoint&view

--- backend/registration/migrations/0002_registration_user.py
+++ backend/registration/migrations/0002_registration_user.py
@@ -10,14 +10,14 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('restaurant', '0001_initial'),
+        ('registration', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='restaurant',
-            name='owner',
-            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='my_restaurants', to=settings.AUTH_USER_MODEL),
+            model_name='registration',
+            name='user',
+            field=models.OneToOneField(null=True, on_delete=django.db.models.deletion.CASCADE, related_name='registration_profile', to=settings.AUTH_USER_MODEL),
         ),
     ]

5239d45dcaeee433b490d3d96788110f976dbd3c
Juan Miguel Sánchez Arce <jumisan134@gmail.com>
2021-03-30 16:33:28 +0200

    Add home (best rated restaurants) view and serializer

--- backend/restaurant/serializers/serializers_main.py
+++ backend/restaurant/serializers/serializers_main.py
@@ -1,6 +1,7 @@
 import json
 
 import jsonpickle
+from django.http import JsonResponse
 from rest_framework import serializers
 from rest_framework.response import Response
 
@@ -59,6 +60,42 @@ class AllCategoriesSerializer(serializers.ModelSerializer):
 
 class BestRatedRestaurantsSerializer(serializers.ModelSerializer):
 
+    # restaurants = Restaurant.objects.all()
+    # ordered_restaurants = sorted(restaurants, key=lambda x: x.average_rating, reverse=True)
+    #
+    # first = serializers.SerializerMethodField()
+    # second = serializers.SerializerMethodField()
+    # third = serializers.SerializerMethodField()
+    # fourth = serializers.SerializerMethodField()
+    #
+    # def get_first(self, instance):
+    #     id = self.ordered_restaurants[0].id
+    #     restaurant = list(Restaurant.objects.filter(id=id).values())
+    #     response = {'id': restaurant.id,
+    #                  'name': restaurant.name,
+    #                  'city': restaurant.city,
+    #                  'categories': restaurant.categories,
+    #                  'price_level': restaurant.price_level,
+    #                  'average_rating': restaurant.average_rating
+    #                  }
+    #     return json.dumps(list(restaurant))
+    #
+    #
+    # def get_second(self, instance):
+    #     id = self.ordered_restaurants[0].id
+    #     restaurant = Restaurant.objects.get(id=id)
+    #     pass
+    #
+    # def get_third(self, instance):
+    #     id = self.ordered_restaurants[0].id
+    #     restaurant = Restaurant.objects.get(id=id)
+    #     pass
+    #
+    # def get_fourth(self, instance):
+    #     id = self.ordered_restaurants[0].id
+    #     restaurant = Restaurant.objects.get(id=id)
+    #     pass
+
     best_four = serializers.SerializerMethodField()
 
     # owner = UserSerializerBasic(read_only=True)
@@ -67,13 +104,19 @@ class BestRatedRestaurantsSerializer(serializers.ModelSerializer):
     def get_best_four(self, instance):
         restaurants = Restaurant.objects.all()
         ordered_restaurants = sorted(restaurants, key=lambda x: x.average_rating, reverse=True)
-        best_four = ordered_restaurants[:4]
-        return Response(jsonpickle.encode(best_four), status=200)
+        best_four_restaurants = ordered_restaurants[:4]
+        # best_four_ids = [ordered_restaurants[0].id,
+        #                  ordered_restaurants[1].id,
+        #                  ordered_restaurants[2].id,
+        #                  ordered_restaurants[3].id]
+        # best_four_restaurants = Restaurant.objects.filter(id__in=best_four_ids).all()
+        return RestaurantSerializer(best_four_restaurants, many=True).data
 
     class Meta:
         model = Restaurant
         fields = ['best_four']
 
+        # fields = ['first', 'second', 'third', 'fourth']
         # fields = ['id',
         #           'name',
         #           'owner',

fdf54174590f85a43e6764dc781e273fdf5e028d
Tenzin Namdak <tennam67@gmail.com>
2021-03-31 10:10:04 +0200

    edited nav profileHandler

--- frontend/src/components/headerNavi/index.jsx
+++ frontend/src/components/headerNavi/index.jsx
@@ -42,7 +42,7 @@ const HeaderNavi = () => {
             <RightHeaderSection>
                 <StyledTab title='home' onClick={homeHandler}>Home</StyledTab>
                 <StyledTab title='search' onClick={searchHandler}>Search</StyledTab>
-                <StyledTab title='profile' onClick={(event) => tabHandler(event)}>Profile</StyledTab>
+                <StyledTab title='profile' onClick={(event) => profileHandler(event)}>Profile</StyledTab>
 
                 {
                     token ? <StyledSignoutButton onClick={logoutHandler()}>LOGOUT</StyledSignoutButton> : 

5c7ea31270aa700390c95bd083c4f68a25418665
Juan Miguel Sánchez Arce <jumisan134@gmail.com>
2021-03-31 16:25:51 +0200

    Start working on registration functionlaity

--- frontend/src/components/registration/registrationField.jsx
+++ frontend/src/components/registration/registrationField.jsx
@@ -1,24 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { RegistrationField, RegistrationTitle } from '../../styles/pageStyles/registrationStyles';
+import {signupCodeFetch} from "../../store/fetches/signup_fetches";
 
 
 //############################# Component ################################
 
 
-const RegistrationFieldComponent = () => {
+const RegistrationFieldComponent = (props) => {
+
+    const [ email, setEmail ] = useState('');
+
+    const sendEmail = () => {
+
+        signupCodeFetch(email)
+            .then(data => {
+                console.log('success!')
+            })
+
+        // loginFetch(email, password)
+        // .then(data => {
+        //     const action = loginAction(data.access);
+        //     dispatch(action);
+        //     localStorage.setItem('token', data.access);
+        //     setRedirect(true);
+        // })
+        // .catch(() => {
+        //         setErrorMessage(true);
+        //     })
+
+        // props.setStage('email sent')
+    }
+
     return (
         <>
                 <RegistrationTitle>Registration <span></span></RegistrationTitle>
                 <RegistrationField>
                     <input
                         required
-                        // onChange={}
-                        // value={}
+                        onChange={event => setEmail(event.target.value)}
                         name='email'
                         type='text'
                         placeholder='E-Mail address'
                     />
-                    <button type='submit'>Register</button>
+                    <button onClick={sendEmail}>Register</button>
                 </RegistrationField>
         </>
     )
